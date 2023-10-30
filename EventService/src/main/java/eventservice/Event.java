bad693a67e33b704c1e497e76dd2fcf6896baafc
Benjamin Flock <bcflock@gmail.com>
2023-10-30 15:32:58 -0400

    implemented EventService(needs proper testing)

---
+++ EventService/src/main/java/eventservice/Event.java
@@ -0,0 +1,93 @@
+package eventservice;
+
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Optional;
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+@Entity("events")
+public record Event(
+        @Id UUID uuid,
+        LocalDateTime eventDateTime,
+        String title,
+        String description,
+        String hEmail) {
+    private static final int TITLE_MAX_LENGTH = 255;
+    private static final int DESCRIPTION_MAX_LENGTH = 600;
+    //returns the LocalDateTime if the date and the time are valid
+    private static LocalDateTime validateDateTime(String date, String time) throws HandledIllegalValueException {
+        if (!date.matches("\\d{4}-\\d{2}-\\d{2}")){
+            throw new HandledIllegalValueException("Date must be formatted YYYY-MM-DD, was " + date);
+        }
+        if (!time.matches("\\d{1,2}:\\d{1,2}")){
+            throw new HandledIllegalValueException("Time must be in the format \"HH:mm\", was " + time);
+        }
+        String[] dateSplit = date.split("-");
+        String[] timeSplit = time.split(" |:");
+        int hour = Integer.parseInt(timeSplit[0]);
+        if (hour < 1 || hour > 24){
+            throw new HandledIllegalValueException("Hour must be in the range [1,24]");
+        }
+        int minute = Integer.parseInt(timeSplit[1]);
+        if(minute < 0 || minute > 59){
+            throw new HandledIllegalValueException("Minutes must be in the range [0, 59]");
+        }
+        int year = Integer.parseInt(dateSplit[0]);
+        Month month = Month.of(Integer.parseInt(dateSplit[1]));
+        int day = Integer.parseInt(dateSplit[2]);
+        return LocalDateTime.of(LocalDate.of(year, month, day), LocalTime.of(hour, minute));
+    }
+    public static String validateEmail(String email) throws HandledIllegalValueException {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        // note -- the actual email regex is much more sophisticated than this,
+        // but there's no good way to add all the proper escapes for java, and
+        // java has no raw strings
+        if(!email.matches(emailRegex)){
+            throw new HandledIllegalValueException("Email %s is invalid".formatted(email));
+        }
+        return email;
+    }
+    public Event validateEmail() throws HandledIllegalValueException{
+        //these should be handled
+        if (title.length() > TITLE_MAX_LENGTH) {
+            throw new HandledIllegalValueException(String.format("Title may not exceed %d", TITLE_MAX_LENGTH));
+        }
+        if (description.length() > DESCRIPTION_MAX_LENGTH) {
+            throw new HandledIllegalValueException(String.format("Description may not exceed %d", DESCRIPTION_MAX_LENGTH));
+        }
+
+        return this;
+    }
+    private static Event create(Optional<String> uuid, String date, String time, String title, String description, String hEmail) throws HandledIllegalValueException{
+        UUID uid;
+        try {
+            uid = uuid.map(UUID::fromString).orElse(UUID.randomUUID());
+        } catch (IllegalArgumentException e) {
+            throw new HandledIllegalValueException("UUID is invalid, please try again");
+        }
+        return new Event(uid, validateDateTime(date, time), title, description, validateEmail(hEmail)).validateEmail();
+    }
+
+    public static Event create(String uuid, String date, String time, String title, String description, String hEmail) throws HandledIllegalValueException{
+        return Event.create(Optional.ofNullable(uuid), date, time, title, description, hEmail).validateEmail();
+    }
+    public static class HandledIllegalValueException extends Exception{
+        public HandledIllegalValueException(String message) {
+            super(message);
+        }
+    }
+
+    @Deprecated
+    public static Event create(String date, String time, String title, String description, String hEmail) throws HandledIllegalValueException{
+        return Event.create(UUID.randomUUID().toString(), date, time, title, description, validateEmail(hEmail));
+    }
+
+}
--- EventService/src/main/java/eventservice/Main.java
+++ EventService/src/main/java/eventservice/Main.java
@@ -1,7 +1,156 @@
 package eventservice;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.sql.SQLException;
+import java.util.*;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import dev.morphia.query.Sort;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import dev.morphia.query.FindOptions;
+
 public class Main {
-    public static void main(String[] args) {
+    private final static String MONGO_URI = "mongodb+srv://%s:%s@%s/"
+            .formatted(
+                    System.getenv("MONGO_USER"),
+                    System.getenv("MONGO_PASS"),
+                    System.getenv("MONGO_URL")
+            );
+    private final static String DATABASE = "";
+    final static Datastore datastore = Morphia.createDatastore(MongoClients.create(MONGO_URI), DATABASE);
+    private static HttpServer server;
+    private final static String HOSTNAME = "localhost";
+    private final static int PORT = 3000;
+    private static Optional<JSONObject> readJSONRequest(HttpExchange exchange) {
+        InputStreamReader r = new InputStreamReader(exchange.getRequestBody());
+        JSONParser parser = new JSONParser();
+        try {
+            return Optional.of((JSONObject) parser.parse(r));
+        } catch (IOException | ParseException e) {
+            // TODO: handle
+            return Optional.empty();
+        }
+    }
+
+    private static void sendJSONResponse(HttpExchange exchange, JSONObject response) {
+        try {
+            exchange.sendResponseHeaders(200, 0);
+            var output = exchange.getResponseBody();
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
+            output.write(response.toJSONString().getBytes());
+        } catch (IOException e) {
+            // TODO: handle
+        }
+        exchange.close();
+    }
+
+    private static void sendResponse(HttpExchange exchange) {
+        sendJSONResponse(exchange, new JSONObject());
+    }
+
+    private static void fail(HttpExchange exchange, int statusCode, String message){
+        System.err.println(message);
+        try {
+            Map<String, String> responseMap = new HashMap<>();
+            responseMap.put("error", message);
+            JSONObject response = new JSONObject(responseMap);
+            exchange.sendResponseHeaders(statusCode, 0);
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
+            var output = exchange.getResponseBody();
+            output.write(response.toJSONString().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        exchange.close();
+    }
+    private static class ListHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) {
+            List<Event> events = datastore.find(Event.class)
+                    .iterator(new FindOptions().sort(Sort.descending("eventDateTime")))
+                    .toList();
+            JSONArray eventList = new JSONArray();
+            events.forEach(
+                    event ->
+                            eventList.add(
+                                    new JSONObject(Map.of(
+                                            "date", event.eventDateTime().toLocalDate(),
+                                            "time", event.eventDateTime().toLocalTime(),
+                                            "title", event.title(),
+                                            "desc", event.description(),
+                                            "email", event.hEmail(),
+                                            "uuid", event.uuid()
+                                    ))
+                            )
+            );
+            sendJSONResponse(exchange, new JSONObject(Map.of("events", eventList)));
+        }
+
+    }
+    private static class AddHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) {
+            try {
+                JSONObject json = readJSONRequest(exchange).orElseThrow();
+
+                String date         = (String) json.get("date");
+                String time         = (String) json.get("time");
+                String title        = (String) json.get("title");
+                String description  = (String) json.get("desc");
+                String hostEmail    = (String) json.get("email");
+                String eventID      = (String) json.get("uuid");
+
+                Event event = Event.create(eventID, date, time, title, description, hostEmail);
+                System.out.println(event);
+
+                datastore.insert(event);
+                System.out.println("Event created: " + event);
+            } catch(NoSuchElementException e){
+                fail(exchange, 400, "Must provide json in request body");
+                return;
+            } catch (Event.HandledIllegalValueException e) {
+                fail(exchange, 400, "Failed to create event: " + e.getMessage());
+                return;
+            }
+            sendResponse(exchange);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        datastore.getMapper().mapPackage("com.mongodb.morphia.entities");
+        datastore.ensureIndexes();
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(HOSTNAME, PORT), 0);
+        } catch (IOException e) {
+            System.err.println("Failed to start server: " + e.getMessage());
+            return;
+        }
         
+        server.createContext("/api/list-events",        new ListHandler());
+        server.createContext("/api/event",              new AddHandler());
+
+        server.start();
+        System.out.println("Server started");
     }
 }
+
