7c3dbda8a5806e3b93707ceb3efbb07ffc8a8ee6
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2023-10-30 18:08:39 -0400

    24H time, update to toaster notifications

--- WebClient/src/App.js
+++ WebClient/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navigator from './components/Navigator';
 import Event from "./pages/Event";
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
     return (
         <div className="App" style={{'margin': '0'}}>
+            <Toaster / >
             <Navigator setPage={setPage} / >
             {page}
         </div>
